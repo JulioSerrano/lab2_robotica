d077261ad6ea885f0ba0072c3cb3144f0283d138
juanpablo <jpmartinez7480@gmail.com>
2018-12-16 18:43:32 -0300

    vista para set pid y metodos bluetooth

---
+++ app/src/main/java/com/example/juanpablo/batboti/CalibrarActivity.java
@@ -0,0 +1,232 @@
+package com.example.juanpablo.batboti;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothSocket;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+
+public class CalibrarActivity extends AppCompatActivity {
+
+    BluetoothSocket mbluetoothSocket;
+
+    TextView kp;
+    TextView kd;
+    TextView ki;
+    TextView sp;
+
+    private static SeekBar kpBar;
+    SeekBar kdBar;
+    SeekBar kiBar;
+    SeekBar spBar;
+
+    int MAX_KP = 100;
+    int MIN_KP = 0;
+    int MAX_KD = 2;
+    int MIN_KD = 0;
+    int MAX_KI = 200;
+    int MIN_KI = 0;
+    int MAX_SP = 360;
+    int MIN_SP = 0;
+
+
+    public CalibrarActivity(BluetoothSocket bs){
+        this.mbluetoothSocket = bs;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calibrate_menu);
+
+        kpBarConfig();
+        kdBarConfig();
+        kiBarConfig();
+        spBarConfig();
+
+    }
+
+
+
+    public void kpBarConfig(){
+        kpBar = (SeekBar)findViewById(R.id.kp_bar);
+        kp = (TextView)findViewById(R.id.kp_text);
+        kp.setText(kpBar.getProgress()+"/100");
+
+        kpBar.setOnSeekBarChangeListener(
+
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progressValue;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progressValue = progress;
+                        kp.setText(progress+"/100");
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        kp.setText(progressValue+"/100");
+                        setKp();
+                    }
+                }
+        );
+    }
+
+    public void kdBarConfig(){
+        kdBar = (SeekBar)findViewById(R.id.kd_bar);
+        kd = (TextView)findViewById(R.id.kd_text);
+        kd.setText(kdBar.getProgress()+"/2");
+
+        kdBar.setOnSeekBarChangeListener(
+
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progressValue;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progressValue = progress;
+                        kd.setText(progress+"/2");
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        kd.setText(progressValue+"/2");
+                        setKd();
+                    }
+                }
+        );
+    }
+
+    public void kiBarConfig(){
+        kiBar = (SeekBar)findViewById(R.id.ki_bar);
+        ki = (TextView)findViewById(R.id.ki_text);
+        ki.setText(kiBar.getProgress()+"/200");
+
+        kiBar.setOnSeekBarChangeListener(
+
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progressValue;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progressValue = progress;
+                        ki.setText(progress+"/200");
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        ki.setText(progressValue+"/200");
+                        setKi();
+                    }
+                }
+        );
+    }
+
+    public void spBarConfig(){
+        spBar = (SeekBar)findViewById(R.id.sp_bar);
+        sp = (TextView)findViewById(R.id.sp_text);
+        sp.setText(spBar.getProgress()+"/360");
+
+        spBar.setOnSeekBarChangeListener(
+
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progressValue;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progressValue = progress;
+                        sp.setText(progress+"/360");
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        sp.setText(progressValue+"/360");
+                        setSp();
+                    }
+                }
+        );
+    }
+
+    private void setKp(){
+        if(mbluetoothSocket!=null){
+            try{
+                mbluetoothSocket.getOutputStream().write(String.valueOf(kpBar.getProgress()).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setKd(){
+        if(mbluetoothSocket!=null){
+            try{
+                mbluetoothSocket.getOutputStream().write(String.valueOf(kdBar.getProgress()).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setKi(){
+        if(mbluetoothSocket!=null){
+            try{
+                mbluetoothSocket.getOutputStream().write(String.valueOf(kiBar.getProgress()).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setSp(){
+        if(mbluetoothSocket!=null){
+            try{
+                mbluetoothSocket.getOutputStream().write(String.valueOf(spBar.getProgress()).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/juanpablo/batboti/MainActivity.java
+++ app/src/main/java/com/example/juanpablo/batboti/MainActivity.java
@@ -3,38 +3,54 @@ package com.example.juanpablo.batboti;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.internal.NavigationMenu;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     ProgressDialog mProgressDialog;
     private static final int REQUEST_ENABLE_BT = 1;
 
     BluetoothAdapter mBluetoothAdapter;
     Set<BluetoothDevice> pairedDevices;
+    BluetoothSocket mbluetoothSocket;
 
     TextView bluetoothMessage;
     Button   btnManual;
     Button   btnAuto;
     Button   btnConnect;
     ListView bluetoothList;
-
+    NavigationView nav;
     ArrayList mDeviceListBluetooth;
 
     ArrayAdapter<String> mArrayAdapter;
+
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    String address = null;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +60,15 @@ public class MainActivity extends AppCompatActivity {
         btnManual = (Button)findViewById(R.id.btnLearn);
         btnAuto = (Button)findViewById(R.id.btnAuto);
         bluetoothList = (ListView)findViewById(R.id.bluetooths);
+        nav = (NavigationView)findViewById(R.id.nav_view);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(!mBluetoothAdapter.isEnabled()){
             setBluetoothState(1);
         }
         else if(mBluetoothAdapter == null)
             setBluetoothState(3);
+
+        nav.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
 
     }
 
@@ -79,8 +98,9 @@ public class MainActivity extends AppCompatActivity {
                     for (BluetoothDevice device : pairedDevices)
                         // Add the name and address to an array adapter to show in a ListView
                         mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                    //bluetoothList.setVisibility(View.VISIBLE);
-                    //bluetoothList.setAdapter(mArrayAdapter);
+                    bluetoothList.setVisibility(View.VISIBLE);
+                    bluetoothList.setAdapter(mArrayAdapter);
+                    bluetoothList.setOnItemClickListener(myTouchListener);
                 }
             }
 
@@ -88,6 +108,37 @@ public class MainActivity extends AppCompatActivity {
                 setBluetoothState(1);
         }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        int id = item.getItemId();
+        switch(id){
+            case R.id.calibrar:
+                calibrarView(null);
+                break;
+        }
+        return false;
+    }
+
+    public void calibrarView(View view){
+        CalibrarActivity ca = new CalibrarActivity(mbluetoothSocket);
+        Intent i = new Intent(this,ca.getClass());
+        startActivity(i);
+    }
+
+    private AdapterView.OnItemClickListener myTouchListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String info = ((TextView) view).getText().toString();
+            address = info.substring(info.length() - 17);
+            try {
+                doConnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
 
     private void setBluetoothState(int c){
         switch(c){
@@ -100,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
               break;
 
             case 2:
-              bluetoothMessage.setText("Buscando...");
+              bluetoothMessage.setText("Conectado");
               bluetoothMessage.setTextColor(Color.GREEN);
               btnManual.setEnabled(true);
               btnAuto.setEnabled(true);
@@ -114,6 +165,67 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException{
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+    }
+
+    public void doConnect() throws IOException {
+
+        try{
+            BluetoothDevice dispositivo = mBluetoothAdapter.getRemoteDevice(address);//connects to the device's address and checks if it's available
+            mbluetoothSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(BTMODULEUUID);//create a RFCOMM (SPP) connection
+            BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+            mbluetoothSocket.connect();//start connection
+            bluetoothList.setVisibility(View.INVISIBLE);
+            setBluetoothState(2);
+        }
+        catch(Exception e){
+            Toast.makeText(this, "No se pudo realizar la conexión", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void foward(View view){
+        if(mbluetoothSocket!=null){
+            try{
+                mbluetoothSocket.getOutputStream().write(String.valueOf(1).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void toRight(View view){
+        if(mbluetoothSocket!=null){
+            try{
+                mbluetoothSocket.getOutputStream().write(String.valueOf(2).getBytes());
+            } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+    }
+
+    public void toLeft(View view){
+        if(mbluetoothSocket!=null){
+            try{
+                mbluetoothSocket.getOutputStream().write(String.valueOf(3).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void down(View view){
+        if(mbluetoothSocket!=null){
+            try{
+                mbluetoothSocket.getOutputStream().write(String.valueOf(4).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
 
 }
