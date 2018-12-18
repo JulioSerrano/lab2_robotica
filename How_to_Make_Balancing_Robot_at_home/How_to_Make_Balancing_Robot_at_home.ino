a01570bbe8714118365e91b1e7f2b90fb54a043b
tomas <tomas.gutierrez.l@usach.cl>
2018-12-18 15:21:08 -0300

    Cleaned and indented code.

--- How_to_Make_Balancing_Robot_at_home/How_to_Make_Balancing_Robot_at_home.ino
+++ How_to_Make_Balancing_Robot_at_home/How_to_Make_Balancing_Robot_at_home.ino
@@ -4,10 +4,8 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 #include <SoftwareSerial.h>
 
-
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-
-#include "Wire.h"
+  #include "Wire.h"
 #endif
 
 #define MIN_ABS_SPEED 30
@@ -31,42 +29,16 @@ Quaternion q; // [w, x, y, z] quaternion container
 VectorFloat gravity; // [x, y, z] gravity vector
 float ypr[3]; // [yaw, pitch, roll] yaw/pitch/roll container and gravity vector
 
-#define  Left_Enoder     0x01
-#define  Right_Enoder    0x02
-#define  Left_PWM        0x03
-#define  Right_PWM       0x04
-#define  Balance_Angle   0x05
-#define  Upright_Kp      0x06
-#define  Upright_Ki      0x07
-#define  Upright_Kd      0x08
-#define  Speed_Kp        0x09
-#define  Speed_Ki        0x10
-#define  Speed_Kd        0x0A
-#define  Rotate_Kp       0x0B
-#define  Rotate_Ki       0x0C
-#define  Rotate_Kd       0x0D
-#define  Contrl_val      0x0E
-#define  angle_output    0x0F
-#define  speed_output    0x11
-
-unsigned char a[12]={0xAA,0xAA,0xAA,0xAA,0x00,0x00,0x00,0x00,0xFF,0xFF,0xFF,0xFF};
-unsigned char RxBuf[12]={0};
-
 //PID
 double originalSetpoint = 178;//165 lo mueve
 double setpoint = originalSetpoint;
 double movingAngleOffset = 0.1;
 double input, output, outputSpeed, inputSpeed;
 
-//adjust these values to fit your own design
-double Kp = 30; // 0 - 100   
+double Kp = 30; // 0 - 100
 double Kd = 1.8; // 0 - 200
 double Ki = 135; // 0 - 2
 PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
-double KpSpeed = 45; // 0 - 100   
-double KdSpeed = 1.8; // 0 - 200
-double KiSpeed = 135; // 0 - 2
-PID pidSpeed(&inputSpeed, &outputSpeed, &setpoint, KpSpeed, KiSpeed, KdSpeed, DIRECT);
 
 double turnChange;
 double motorSpeedFactorLeft = 0.60; //lenta
@@ -83,72 +55,53 @@ int i= 0;
 unsigned char aux4[2];
 boolean flag = true;
 
-
-
 LMotorController motorController(ENA, IN1, IN2, ENB, IN3, IN4, motorSpeedFactorLeft, motorSpeedFactorRight);
 
-volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
-void dmpDataReady()
-{
-mpuInterrupt = true;
+volatile bool mpuInterrupt = false;
+
+void dmpDataReady() {
+  mpuInterrupt = true;
 }
 
+void setup() {
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
 
-void setup()
-{
-// join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-Wire.begin();
-TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-Fastwire::setup(400, true);
-#endif
+  mpu.initialize();
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  devStatus = mpu.dmpInitialize();
 
-mpu.initialize();
-Serial.begin(9600);
-bluetooth.begin(9600);
-devStatus = mpu.dmpInitialize();
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1688); // 1688 factory default for my test chip
 
-// supply your own gyro offsets here, scaled for min sensitivity
-mpu.setXGyroOffset(220);
-mpu.setYGyroOffset(76);
-mpu.setZGyroOffset(-85);
-mpu.setZAccelOffset(1688); // 1688 factory default for my test chip
-
-// make sure it worked (returns 0 if so)
-if (devStatus == 0)
-{
-// turn on the DMP, now that it's ready
-mpu.setDMPEnabled(true);
-
-// enable Arduino interrupt detection
-attachInterrupt(0, dmpDataReady, RISING);
-mpuIntStatus = mpu.getIntStatus();
-
-// set our DMP Ready flag so the main loop() function knows it's okay to use it
-dmpReady = true;
-
-// get expected DMP packet size for later comparison
-packetSize = mpu.dmpGetFIFOPacketSize();
-
-//setup PID
-pid.SetMode(AUTOMATIC);
-pid.SetSampleTime(10);
-pid.SetOutputLimits(-255, 255); 
+  if (devStatus == 0) {
+    mpu.setDMPEnabled(true);
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    dmpReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize();
+    //setup PID
+    pid.SetMode(AUTOMATIC);
+    pid.SetSampleTime(10);
+    pid.SetOutputLimits(-255, 255);
+  }else {
+  // ERROR!
+  // 1 = initial memory load failed
+  // 2 = DMP configuration updates failed
+  Serial.print(F("DMP Initialization failed (code "));
+  Serial.print(devStatus);
+  Serial.println(F(")"));
+  }
 }
-else
-{
-// ERROR!
-// 1 = initial memory load failed
-// 2 = DMP configuration updates failed
-// (if it's going to break, usually the code will be 1)
-Serial.print(F("DMP Initialization failed (code "));
-Serial.print(devStatus);
-Serial.println(F(")"));
-}
-}
-void forward()
-{
+
+void forward() {
   double newSetpoint = originalSetpoint - 0.5;
   //pid.setKp(Kp+5);
   //motorController.move(output*1.5,MIN_ABS_SPEED);
@@ -158,13 +111,12 @@ void forward()
   newSetpoint = originalSetpoint + 0.5;
   pid.setpoint(&newSetpoint);
 }
-void backward()
-{
-  
+
+void backward() {
+
 }
 
-void turnLeft()
-{
+void turnLeft() {
   turnChange = 0.65; //lenta
   motorSpeedFactorRight = turnChange;
   //motorController.move(output*10, output*5, MIN_ABS_SPEED);
@@ -173,23 +125,19 @@ void turnLeft()
   motorSpeedFactorRight = 0.55;
   motorSpeedFactorLeft = 0.55;
 }
-void turnRight()
-{
+
+void turnRight() {
   motorController.move(output*5, output*10, MIN_ABS_SPEED);
 }
 
-void loop()
-{
+void loop() {
 // if programming failed, don't try to do anything
-if (!dmpReady) return;
+  if (!dmpReady) return;
 
-// wait for MPU interrupt or extra packet(s) available
-// wait for MPU interrupt or extra packet(s) available
-  if(bluetooth.available() > 0){
+  if(bluetooth.available() > 0) {
     //aux3 = bluetooth.read();
     bluetooth.readBytes(aux4,2);
-    switch(aux4[0])
-    {
+    switch(aux4[0]) {
       case 1:
         Serial.println("Adelante!" + String(aux4[1]));
         forward();
@@ -230,55 +178,28 @@ if (!dmpReady) return;
     }
   }
 
+  while (!mpuInterrupt && fifoCount < packetSize) {
+    pid.Compute();
+    motorController.move(output,MIN_ABS_SPEED);
+  }
 
-while (!mpuInterrupt && fifoCount < packetSize)
-{
-//no mpu data - performing PID calculations and output to motors 
-        pid.Compute();
-        motorController.move(output,MIN_ABS_SPEED);
-        /*if(output < 25 && output > 0 ){ // cuando se encuentra estable(vertical) aumenta la velocidad de la ruedas
-          //Serial.println(output);
-          motorController.move(output*10,MIN_ABS_SPEED);
-        }
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+  fifoCount = mpu.getFIFOCount();
 
-        else{
-           motorController.move(output,MIN_ABS_SPEED);
-        }*/
-   
-}
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+  }else if (mpuIntStatus & 0x02) {
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
 
-// reset interrupt flag and get INT_STATUS byte
-mpuInterrupt = false;
-mpuIntStatus = mpu.getIntStatus();
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    fifoCount -= packetSize;
 
-// get current FIFO count
-fifoCount = mpu.getFIFOCount();
-
-// check for overflow (this should never happen unless our code is too inefficient)
-if ((mpuIntStatus & 0x10) || fifoCount == 1024)
-{
-// reset so we can continue cleanly
-mpu.resetFIFO();
-Serial.println(F("FIFO overflow!"));
-
-// otherwise, check for DMP data ready interrupt (this should happen frequently)
-}
-else if (mpuIntStatus & 0x02)
-{
-// wait for correct available data length, should be a VERY short wait
-while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-// read a packet from FIFO
-mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-// track FIFO count here in case there is > 1 packet available
-// (this lets us immediately read more without waiting for an interrupt)
-fifoCount -= packetSize;
-
-mpu.dmpGetQuaternion(&q, fifoBuffer);
-mpu.dmpGetGravity(&gravity, &q);
-mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-input = ypr[1] * 180/M_PI + 180;
-inputSpeed = ypr[1] * 180/M_PI + 180;
-}
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    input = ypr[1] * 180/M_PI + 180;
+    inputSpeed = ypr[1] * 180/M_PI + 180;
+  }
 }
